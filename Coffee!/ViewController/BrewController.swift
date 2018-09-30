ca38fa5166344dbbb137d3dfa27ad2c3c5ddfe41
Hana Kaspříková <kasprikova.hana@gmail.com>
2018-10-01 00:19:17 +0200

    Design update

--- Coffee!/ViewController/BrewController.swift
+++ Coffee!/ViewController/BrewController.swift
@@ -10,14 +10,14 @@ import UIKit
 
 class BrewController: UIViewController {
     
-    @IBOutlet weak var literLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var coffeeLabel: UILabel!
     @IBOutlet weak var waterNeededLabel: UILabel!
     @IBOutlet weak var brewTimeLabel: UILabel!
     @IBOutlet weak var waterLabel: UILabel!
-    @IBOutlet weak var ozLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     
-    var grams = 0
+    var grams = 30
     var water = 0
     
     var method: PreparationMethod?
@@ -29,8 +29,66 @@ class BrewController: UIViewController {
 
         setTransparentNavigationBar()
         
-        evaluate()
+        updateCoffeeLabelValue(value: grams)
+        
         setUpLabels()
+    }
+    
+    //MARK: - Custom functions
+    
+    func createAlert(title: String, message: String, value: Int, includeCloseAlert: Bool) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: Constants.defaultStyleTitle, style: .default, handler: {action in self.setGrams(value: value)}))
+        if(includeCloseAlert) {
+            alert.addAction(UIAlertAction(title: Constants.cancelStyleTitle, style: .cancel, handler: nil))
+        }
+        
+        present(alert, animated: true)
+    }
+    
+    func updateCoffeeLabelValue(value: Int) {
+        coffeeLabel.text = String(value)
+        evaluate()
+    }
+    
+    func setGrams(value: Int) {
+        grams = value
+        updateCoffeeLabelValue(value: grams)
+    }
+    
+    //MARK: - Actions
+    
+    @IBAction func tapDown(_ sender: UIButton) {
+        grams -= 1
+        setGrams(value: grams)
+        
+        switch grams {
+        case 0:
+            createAlert(title: Constants.noCoffeeTitle, message: Constants.noCoffeeMessage, value: 1, includeCloseAlert: false)
+            break
+        case 14:
+            createAlert(title: Constants.areYouSureTitle, message: Constants.notEnoughCoffeeMessage, value: 15, includeCloseAlert: true)
+            break
+        default:
+            break
+        }
+    }
+    
+    @IBAction func tapUp(_ sender: UIButton) {
+        grams += 1
+        setGrams(value: grams)
+        
+        switch grams {
+        case 46:
+            createAlert(title: Constants.areYouSureTitle, message: Constants.aLotOfCoffeeMessage, value: 30, includeCloseAlert: true)
+            break
+        case 99:
+            createAlert(title: Constants.tooMuchTitle, message: Constants.tooMuchCoffeeMessage, value: 30, includeCloseAlert: false)
+            break
+        default:
+            break
+        }
     }
 }
 
@@ -43,20 +101,28 @@ extension BrewController {
             case .frenchPress:
                 water = Int(round(Float(grams)*(900/60)))
         }
+        
+        waterLabel.text = String(water)
     }
 
     fileprivate func setUpLabels() {
-        waterNeededLabel.text = "WATER NEEDED"
-        brewTimeLabel.text = "BREW TIME"
-        ozLabel.text = "~" + String(water/28) + " oz"
-        literLabel.text = "~" + String(Float(water)/1000) + " liter"
-        waterLabel.text = String(water)
+        waterNeededLabel.text = "Water"
+        brewTimeLabel.text = "Time"
+        
+        //Add later:
+        /*ozLabel.text = "~" + String(water/28) + " oz"
+        literLabel.text = "~" + String(Float(water)/1000) + " liter"*/
         
         switch method ?? .drip {
             case .drip:
-                timeLabel.text = "3 - 4 min."
-            case .frenchPress, .auto:
-                timeLabel.text = "4 min."
+                timeLabel.text = "3 - 4 min"
+                typeLabel.text = "Drip"
+            case .frenchPress:
+                timeLabel.text = "4 min"
+                typeLabel.text = "French Press"
+            case .auto:
+                timeLabel.text = "4 min"
+                typeLabel.text = "Auto"
         }
     }
 }
--- Coffee!/ViewController/ViewController.swift
+++ Coffee!/ViewController/ViewController.swift
@@ -13,9 +13,7 @@ class ViewController: UIViewController {
     //MARK: - Properties
 
     @IBOutlet weak var segmentedControl: UISegmentedControl!
-    @IBOutlet weak var coffeeLabel: UILabel!
     
-    var grams = 30
     private var method: PreparationMethod = .drip
     
     private var selectedSegmentIndex = 0 {
@@ -28,7 +26,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         setTransparentNavigationBar()
-        updateCoffeeLabelValue(value: grams)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -40,65 +37,12 @@ class ViewController: UIViewController {
             return
         }
         
-        vc.grams = grams
         vc.method = method
     }
     
     //MARK: - Custom functions
     
-    func createAlert(title: String, message: String, value: Int, includeCloseAlert: Bool) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        
-        alert.addAction(UIAlertAction(title: Constants.defaultStyleTitle, style: .default, handler: {action in self.setGrams(value: value)}))
-        if(includeCloseAlert) {
-            alert.addAction(UIAlertAction(title: Constants.cancelStyleTitle, style: .cancel, handler: nil))
-        }
-        
-        present(alert, animated: true)
-    }
-    
-    func updateCoffeeLabelValue(value: Int) {
-        coffeeLabel.text = String(value)
-    }
-    
-    func setGrams(value: Int) {
-        grams = value
-        updateCoffeeLabelValue(value: grams)
-    }
-    
     //MARK: - Actions
-
-    @IBAction func tapDown(_ sender: UIButton) {
-        grams -= 1
-        setGrams(value: grams)
-        
-        switch grams {
-        case 0:
-            createAlert(title: Constants.noCoffeeTitle, message: Constants.noCoffeeMessage, value: 1, includeCloseAlert: false)
-            break
-        case 14:
-           createAlert(title: Constants.areYouSureTitle, message: Constants.notEnoughCoffeeMessage, value: 15, includeCloseAlert: true)
-            break
-        default:
-            break
-        }
-    }
-    
-    @IBAction func tapUp(_ sender: UIButton) {
-        grams += 1
-        setGrams(value: grams)
-        
-        switch grams {
-        case 46:
-            createAlert(title: Constants.areYouSureTitle, message: Constants.aLotOfCoffeeMessage, value: 30, includeCloseAlert: true)
-            break
-        case 100:
-            createAlert(title: Constants.tooMuchTitle, message: Constants.tooMuchCoffeeMessage, value: 30, includeCloseAlert: false)
-            break
-        default:
-           break
-        }
-    }
     
     @IBAction func indexChanged(_ sender: Any) {
         selectedSegmentIndex = segmentedControl.selectedSegmentIndex
